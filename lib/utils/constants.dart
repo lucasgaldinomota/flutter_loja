574f8671dead7921de384e25c5cdcebe1d8f8fb6
Lucas Galdino <lucasgaldino7@gmail.com>
2022-08-26 09:22:21 -0300

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,5 @@
 class Constants {
-  static const USER_FAVORITES_URL =
-      'https://shop-cod3r-39781-default-rtdb.firebaseio.com/userFavorites';
-  static const PRODUCT_BASE_URL =
-      'https://shop-cod3r-39781-default-rtdb.firebaseio.com/products';
-  static const ORDER_BASE_URL =
-      'https://shop-cod3r-39781-default-rtdb.firebaseio.com/orders';
+  static const USER_FAVORITES_URL = '';
+  static const PRODUCT_BASE_URL = '';
+  static const ORDER_BASE_URL = '';
 }
